f6afd9786b8e3653bbc6f38dc552188974ea2f8b
Peter Tiernan <ptiernan@gmail.com>
2019-01-14 16:26:24 +0000

    updated about section

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -14,13 +14,18 @@ About Drumcondra FC: One Club - One Community
         <h2>About Us</h2>
         <div class="about-info-text">
             <h3>From the begining... </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo amet, sed culpa voluptas ipsa, ipsum, perspiciatis veritatis
-                minus sint fugiat similique. Officia sequi blanditiis cupiditate delectus rem reiciendis eveniet quo eum nobis, aspernatur
-                aliquid ipsa autem cumque totam, ut voluptatibus. Vitae eos quia asperiores accusamus expedita, vero inventore tenetur
-                excepturi neque. Vel sit voluptates quasi, tempore ut voluptatum at illo labore veritatis odit nam nisi quae doloribus
-                ducimus deleniti cum eos vitae iure nemo id nobis nesciunt architecto, neque repudiandae. Ratione esse nulla laborum
-                velit consectetur voluptas officia adipisci aspernatur iure numquam, rem assumenda quisquam minima suscipit, deserunt
-                nihil neque.</p>
+            <p>Prior to joining the League, Drums became the first non-league club to win the FAI cup in the 1926-27 season, completing an unlikely double by also lifting the Intermediate Cup along the way. Following election to the League however they would endure a twenty-year wait before lifting their first title in 1948. During the 1950’s Drums rivalry with Shamrock Rovers became the stuff of Dublin folklore as huge crowds gathered to see the Northside/Southside derby fixtures between the two clubs. During this halcyon period for Drums the early 1960’s saw the Tolka Park side make history as the first League of Ireland side to register an aggregate win in European competition when they knocked Danish side B 1909 Odense out of the Inter-Cities Fairs Cup (4-1 H, 2-4 A) in the 1962-63 season. Although beaten 6-0 by Bayern Munich in the following round in Germany, they did restore pride with a 1-0 home win in the second leg. Drums also played Atlético Madrid (0-8 A, 1-5 H), Nuremberg (0-5 A, 1-4 H), Eintracht Frankfurt (0-2 H,1-6 A), and Vorwärts Berlin (1-0 H, 0-3 A) in European competition.
+
+                <br><br>
+
+                At the end of the 1971-72 season the team amalgamated with Home Farm, Drums final league game in their own right being a 1-1 draw with Shelbourne at Tolka on 5 April 1972. It was announced by Brendan Menton Sr., Home Farm honorary secretary at the time, that the all-amateur club had taken over the entire share capital of their professional neighbouring club, Drumcondra F.C. Promotion of amateur football was to remain the main aim. During the 1972-73 season the side played as Home Farm-Drumcondra however within a season the Drumcondra name was dropped and from the start of 1973-74 the side simply became Home Farm.
+
+                <br><br>
+                The club have been variously known over the years as Drumcondra FC and Drumcondra AFC. However the name Drumcondra AFC lives on today with the fielding of 14 teams at various age groups from U8’s to adult in the Dublin & District Schoolboy League(DDSL), North Dublin Schoolboy League(NDSL) and the United Churches Football League(UCFL).
+                 
+                <br><br>
+                Our club still play in the traditional blue and gold/yellow strip and play from our base in Clonturk Park opposite our original home of Tolka Park in Drumcondra with other pitches in Griffith Park, Dominican College and Albert College Park.
+            </p>
         </div>
     </section>
 
