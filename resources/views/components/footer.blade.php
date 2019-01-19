f9ef37cd8d1e91b0bfc87841dc59956bffe517ff
Peter Tiernan <ptiernan@gmail.com>
2019-01-19 18:49:55 +0000

    minor edit to footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="footer">
         <div class="info">
-            Made with <span>&hearts;</span> by <a href="http://www.rocketchipwebsolutions.ie" target="_blank">Rocket.Chip WebSolutions</a> 2019
+            Made with <span>&hearts;</span> by <a href="http://www.rocketchipwebsolutions.ie" target="_blank">Rocket.Chip Web Solutions</a> 2019
         </div>
     </div>
